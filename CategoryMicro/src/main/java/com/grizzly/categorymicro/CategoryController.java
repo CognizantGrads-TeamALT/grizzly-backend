9f66a7e67d0f20e174ec1bb0e2577e653b6ef600
jenni <jeniperiasamy@gmail.com>
2018-05-30 10:29:10 +1000

    Category Microservice

--- CategoryMicro/src/main/java/com/grizzly/categorymicro/CategoryController.java
+++ CategoryMicro/src/main/java/com/grizzly/categorymicro/CategoryController.java
@@ -1,40 +1,47 @@
 package com.grizzly.categorymicro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 
 @RestController
+
+@RequestMapping("/category")
 public class CategoryController {
 
-    @RequestMapping("/")
-    public String index() {
-        String spam = new String();
-        spam = "<br/>1 little bug in the code, 1 little bug, take it down, patch it around ";
-        Random r = new Random();
-        int i = 0;
-        while(i < 100){
-            i++;
-            int temp = r.nextInt(1000);
-            spam.concat(temp + " little bugs in the code<br/>" + temp + " little bugs in the code, " + temp + " little bugs, take one down, patch it around ");
+    @Autowired
+    private CategoryService categoryService;
 
+
+    /**
+     * Return a list of all categories in the system
+     * @return categories in a list
+     */
+    @GetMapping("/all/{column_name}")
+    public ResponseEntity<ArrayList<Category>> getAll(@PathVariable(value="column_name") String column_name) {
+        ArrayList<Category> categories = categoryService.getAll(column_name);
+
+        // if no categories found
+        if (categories == null || categories.isEmpty()) {
+            return new ResponseEntity<>(categories, HttpStatus.NOT_FOUND);
         }
-        spam.concat("<br/> finally no bugs in the code! <br/><br/><br/><br/> ...That's the wrong output.");
 
-
-        return "Greetings from Spring Boot!" +
-                "<br/>Lawrence's line (but added via pull request)." +
-                "<br/>Jenni's Line" +           
-                "<br/>Jonathon's line. "+
-                "<br/> Jenni's second attempt" +
-                "<br/>Steve's been here." +
-                "<br/>Sophie was here too" +
-                "<br/>Bentley's been here." +
-                "<br/>Jonathon Returned, Team ALT > Team TS" +
-                "<br/> trying to learning git flow" +
-                spam;
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello()
+    {
+        return new ResponseEntity<String>("Hello!", HttpStatus.OK);
+    }
+
 
 }
