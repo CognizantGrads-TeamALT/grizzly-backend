9e9a2d183a4aedfcda44b8b1ac5d31143f4c7f1a
Steven Phan <thanhviet.phan@cognizant.com>
2018-05-31 11:36:57 +1000

    rename Request to CategoryDTO

---
+++ CategoryMicro/src/main/java/com/grizzly/categorymicro/CategoryDTO.java
@@ -0,0 +1,41 @@
+package com.grizzly.categorymicro;
+
+public class CategoryDTO {
+
+    private String categoryId;
+    private String name;
+    private String description;
+    private boolean enabled;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
