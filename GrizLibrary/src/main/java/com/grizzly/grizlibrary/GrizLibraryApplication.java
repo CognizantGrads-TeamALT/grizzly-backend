4145094133c4074da0a226000daa28c2808764d5
Lawrence MacDonald <lawrence.macdonald@cognizant.com>
2018-06-13 13:09:55 +1000

    Fixed compile issue - boot projects cannot depend on boot projects

--- GrizLibrary/src/main/java/com/grizzly/grizlibrary/GrizLibraryApplication.java
+++ GrizLibrary/src/main/java/com/grizzly/grizlibrary/GrizLibraryApplication.java
@@ -2,10 +2,8 @@ package com.grizzly.grizlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+//@SpringBootApplication
 public class GrizLibraryApplication {
     public static void main(String[] args) {
         SpringApplication.run(GrizLibraryApplication.class, args);
