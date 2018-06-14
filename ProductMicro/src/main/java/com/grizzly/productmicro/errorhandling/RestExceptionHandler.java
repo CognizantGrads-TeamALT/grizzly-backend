dbb9e5249f93cf980b7ffb6430938c8fd868ca1e
Lawrence MacDonald <lawrence.macdonald@cognizant.com>
2018-06-14 14:16:14 +1000

    Added global exception handling to product micro

---
+++ ProductMicro/src/main/java/com/grizzly/productmicro/errorhandling/RestExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.grizzly.productmicro.errorhandling;
+
+import com.grizzly.grizlibrary.errorhandling.ApiError;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.grizzly.grizlibrary.helpers.Helper.buildResponse;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Handles HttpMessageNotReadable exceptions thrown by calling it a malformed JSON request
+     */
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponse(new ApiError(HttpStatus.BAD_REQUEST, e, error));
+    }
+
+    /**
+     * Globally handles IllegalArgumentExceptions thrown
+     */
+    protected ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Information passed to the system was invalid.";
+        return buildResponse(new ApiError(HttpStatus.BAD_REQUEST, e, error));
+    }
+}
