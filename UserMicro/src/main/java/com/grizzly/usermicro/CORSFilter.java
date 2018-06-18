bf0258937d6d7ba584ffc54f781c372875996020
Dan Sali <ramadan.sali@cognizant.com>
2018-06-18 17:38:24 +1000

    I JUST WANT CACHING TO WORK D:

--- UserMicro/src/main/java/com/grizzly/usermicro/CORSFilter.java
+++ UserMicro/src/main/java/com/grizzly/usermicro/CORSFilter.java
@@ -26,7 +26,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, cache-control");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
