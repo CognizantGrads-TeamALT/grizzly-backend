7469d870c31c70533f8d988aaf626a41b6b22b91
Dan Sali <ramadan.sali@cognizant.com>
2018-06-20 17:49:44 +1000

    Test refreshscope

--- ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
+++ ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ProductMicroApplication {
     @Value("${mysql.username}")
     private String username;
