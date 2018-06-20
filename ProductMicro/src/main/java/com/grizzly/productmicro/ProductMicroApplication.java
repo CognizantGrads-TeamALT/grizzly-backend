e8b47e5bea68256c313578a1115318e6aff35124
Dan Sali <ramadan.sali@cognizant.com>
2018-06-20 17:22:50 +1000

    Client test.

--- ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
+++ ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
@@ -1,13 +1,31 @@
 package com.grizzly.productmicro;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProductMicroApplication {
+    @Value("${mysql.username}")
+    private String username;
+
+    @Value("${mysql.password}")
+    private String password;
+
     public static void main(String[] args) {
         SpringApplication.run(ProductMicroApplication.class, args);
+    }
+
+    @RequestMapping(
+            value = "/test",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test() {
+        return username + " " + password;
     }
 }
