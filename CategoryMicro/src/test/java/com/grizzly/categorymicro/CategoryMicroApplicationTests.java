8d6766e9165deeb00a48bb68d9eb901c32fe1c57
dansali1 <39324744+dansali1@users.noreply.github.com>
2018-06-20 11:59:15 +1000

    Update CategoryMicroApplicationTests.java
    re-enable tests without library functions

--- CategoryMicro/src/test/java/com/grizzly/categorymicro/CategoryMicroApplicationTests.java
+++ CategoryMicro/src/test/java/com/grizzly/categorymicro/CategoryMicroApplicationTests.java
@@ -21,66 +21,11 @@ public class CategoryMicroApplicationTests {
 	@Autowired
 	CategoryService testService;
 
-	//@InjectMocks
-	//CategoryService mockService;
+	@InjectMocks
+	CategoryService mockService;
 
 	@Test
 	public void contextLoads() {
-	}
-
-	/*@Test
-	public void makeListFromIterableReplicatesIterables() {
-		// set up
-		List<String> original = new ArrayList<String>();
-		original.add("one");
-		original.add("three");
-		original.add("two");
-		List<String> after;
-
-		// execution
-		after = testService.makeListFromIterable(original);
-
-		// verification
-		assertEquals(original, after);
-	}
-
-	@Test
-	public void makeListFromIterableChangesIterableType() {
-		// set up
-		List<String> expected = new ArrayList<String>();
-		expected.add("one");
-		expected.add("three");
-		expected.add("two");
-
-		List<String> original = new LinkedList<String>();
-		original.add("one");
-		original.add("three");
-		original.add("two");
-
-		ArrayList<String> result;
-
-		// execution
-		result = testService.makeListFromIterable(original);
-
-		// verification
-		assertEquals(expected, result);
-	}
-
-	@Test
-	public void getPageRequest_DefaultstoId() {
-		//set up
-		Integer pageIndex = 0;
-		String column_name = "invalid";
-		String expected = "categoryId: ASC";
-		PageRequest result;
-		String resultString;
-
-		//execution
-		result = testService.getPageRequest(pageIndex, column_name);
-		resultString = result.getSort().toString();
-
-		//verification
-		assertEquals(expected, resultString);
 	}
 
 	@Test
@@ -141,5 +86,5 @@ public class CategoryMicroApplicationTests {
 
 		// verification
 		assertNull(newCat);
-	}*/
+	}
 }
