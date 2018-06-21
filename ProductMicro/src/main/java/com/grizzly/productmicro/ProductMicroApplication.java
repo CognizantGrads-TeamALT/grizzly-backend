bfe625bbb0058b6dee3ea31fbc339adb2dcf8fa7
Dan Sali <ramadan.sali@cognizant.com>
2018-06-21 15:11:46 +1000

    Remove this left over code.

--- ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
+++ ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
@@ -1,21 +1,12 @@
 package com.grizzly.productmicro;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class ProductMicroApplication {
-    @Value("${mysql.username}")
-    private String username;
-
-    @Value("${mysql.password}")
-    private String password;
-
     public static void main(String[] args) {
         SpringApplication.run(ProductMicroApplication.class, args);
     }
