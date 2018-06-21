41e485eb4df51fb0b8972917f38ecda518891b99
Dan Sali <ramadan.sali@cognizant.com>
2018-06-21 16:16:04 +1000

    bootstrap was loading too quick. refreshscope maybe fixes mysql.

--- CategoryMicro/src/main/java/com/grizzly/categorymicro/CategoryMicroApplication.java
+++ CategoryMicro/src/main/java/com/grizzly/categorymicro/CategoryMicroApplication.java
@@ -3,9 +3,11 @@ package com.grizzly.categorymicro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class CategoryMicroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CategoryMicroApplication.class, args);
