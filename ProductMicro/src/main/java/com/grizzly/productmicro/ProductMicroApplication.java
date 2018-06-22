2c676d6b104f878b0b40686e541c30e0e047e010
Dan Sali <ramadan.sali@cognizant.com>
2018-06-22 10:19:31 +1000

    Caching for ImageDTO on the backend test

--- ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
+++ ProductMicro/src/main/java/com/grizzly/productmicro/ProductMicroApplication.java
@@ -2,11 +2,13 @@ package com.grizzly.productmicro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 @RefreshScope
 public class ProductMicroApplication {
     public static void main(String[] args) {
